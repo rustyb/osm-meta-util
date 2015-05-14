3aa6cdf52a923a3dd1eb847eca248db76f113d6e
Colin Broderick <rusty@therustyproject.com>
2015-05-14 19:08:48 +0200

    updated index js to output whether create, update or delete

--- index.js
+++ index.js
@@ -5,121 +5,128 @@ var expat = require('node-expat');
 var Readable = require('stream').Readable;
 var util = require('util');
 
-util.inherits(MetaUtil, Readable)
+util.inherits(MetaUtil, Readable);
 
 function MetaUtil(opts) {
     if (!(this instanceof MetaUtil)) return new MetaUtil(opts);
-    
-    opts = opts || {}
-    Readable.call(this, opts)
-    
+
+    opts = opts || {};
+    Readable.call(this, opts);
+
     var that = this;
-    this.liveMode = (!opts.start && !opts.end && !opts.delay)
+    this.liveMode = (!opts.start && !opts.end && !opts.delay);
     this.state = Number(opts.start) || 0;
     this.end = Number(opts.end) || 1;
-    this.diff = this.end - this.state;
-    this.delay = (opts.delay || 60000)
+    this.diff = this.end - this.state + 1;
+    this.delay = (opts.delay || 60000);
     this.initialized = true;
 
-
-    //this.baseURL = opts.baseURL || 'http://planet.osm.org/replication/changesets'
-    this.baseURL = opts.baseURL || 'http://download.geofabrik.de/africa/lesotho-updates'
-    this._changesetAttrs = {}
+    //this.baseURL = opts.baseURL || 'http://planet.osm.org/replication/changesets';
+    //http://download.geofabrik.de/africa/lesotho-updates
+    this.baseURL = opts.baseURL || 'http://download.geofabrik.de/africa/lesotho-updates'; //'http://download.geofabrik.de/africa/lesotho-updates';
+    this._changesetAttrs = {};
     this.started = false;
-    //start
-
 }
 
 MetaUtil.prototype._read = function() {
     var that = this;
     if (!this.started) {
         if (this.liveMode) {
-            //request.get('http://planet.osm.org/replication/changesets/state.yaml', 
+            //request.get('http://planet.osm.org/replication/changesets/state.yaml',
             request.get('http://download.geofabrik.de/africa/lesotho-updates/state.txt',
             function(err, response, body) {
-                that.state = Number(body.substr(body.length - 8))
-                that.end = Infinity //don't stop
-                that.delay = 60000 //every minute
-                that.run()
+                that.state = Number(body.substr(body.length - 8));
+                that.end = Infinity; //don't stop
+                that.delay = 60000; //every minute
+                that.run();
                 that.started = true;
             }
-        )
+        );
         } else {
-            this.run() 
-            this.started = true
-			//console.log(this)
+            this.run();
+            this.started = true;
         }
     }
-}
+};
 
 MetaUtil.prototype.run = function() {
     var that = this;
-	//console.log(this)
     var numProcessed = 0;
+
+    function queueNext() {
+      that.diff -= 1;
+      if (that.diff > 0 || that.liveMode) {
+        setTimeout(function() {
+          next();
+        }, that.delay);
+      }
+    }
+
     var parserEnd = function(name, attrs) {
-        /*if (name === 'changeset') {
-            that.push(new Buffer(JSON.stringify(that._changesetAttrs) + '\n'), 'ascii');
-        }*/
-        if (name === 'node' || name === 'way') {
-            that.push(new Buffer(JSON.stringify(that._changesetAttrs) + '\n'), 'ascii');
+        if (name === 'node') {
+            that.push(new Buffer(JSON.stringify(that._changesetAttrs) + '\n'), 'utf8');
         }
         if (name === 'osm') {
-            that.diff -= 1;
-            if (that.diff < 0) {
-                that.push(null)
-            }
+          queueNext();
+          if (!that.liveMode && that.diff === 0) {
+              that.push(null);
+          }
         }
-	 
-    }
-
+    };
+    var type = ''
     var parserStart = function(name, attrs) {
-        /*if (name === 'changeset') { 
-            if (attrs) {
-                that._changesetAttrs = attrs;
-            }
-        }*/
-        if (name === 'node' || name === 'way') { 
-            if (attrs) {
-                that._changesetAttrs = attrs;
-            }
+        if (name === 'modify' || name === 'create' || name === 'delete') {
+            type = name;
         }
-        /*if (name === 'tag' && that._changesetAttrs && that._changesetAttrs['open'] === 'false') { 
-            that._changesetAttrs[attrs['k']] = attrs['v'];
-        }*/
-        if (name === 'tag' && that._changesetAttrs) { 
-            that._changesetAttrs[attrs['k']] = attrs['v'];
+        if (name === 'node' || name === 'way') {
+            that._changesetAttrs = attrs;
+            that._changesetAttrs['type'] = type;
         }
-    }
+    };
 
-    var interval = setInterval(function()  {
+    function next()  {
         //Add padding
-        var stateStr = that.state.toString().split('').reverse()
-		//console.log(stateStr.length)
-        var diff = 9 - stateStr.length
-		//console.log(diff)
-        for (var i=0; i < diff; i++) { stateStr.push('0') }
+        var stateStr = that.state.toString().split('').reverse();
+        var diff = 9 - stateStr.length;
+        for (var i=0; i < diff; i++) { stateStr.push('0'); }
         stateStr = stateStr.join('');
 
         //Create URL
         var url = '';
-        for (var i=0; i<(stateStr.length/3); i++) {
-            url += stateStr[i*3] + stateStr[i*3 + 1] + stateStr[i*3 + 2] + '/'
+        for (i=0; i<(stateStr.length/3); i++) {
+            url += stateStr[i*3] + stateStr[i*3 + 1] + stateStr[i*3 + 2] + '/';
         }
-    
+
         //XML Parser
         var xmlParser = new expat.Parser('UTF-8');
-        xmlParser.on('startElement', parserStart)
-        xmlParser.on('endElement', parserEnd)
+        xmlParser.on('startElement', parserStart);
+        xmlParser.on('endElement', parserEnd);
 
-        request.get(that.baseURL + url.split('').reverse().join('') + '.osc.gz')
-            .pipe(zlib.createUnzip())
-            .pipe(xmlParser)
+        //Get YAML state file
+        request.get('http://planet.osm.org/replication/changesets/state.yaml',
+            function(err, response, body) {
+                var nodata = true;
+                //If YAML state is bigger, we can get a new file
+                if (Number(body.substr(body.length - 8)) >= that.state) {
+                    var ss = request.get(that.baseURL + url.split('').reverse().join('') + '.osc.gz')
+                        .pipe(zlib.createUnzip())
+                        .on('data', function(data) {
+                          nodata = (data.length === 0) && nodata;
+                        })
+                        .on('end', function() {
+                          if (nodata) {
+                            queueNext();
+                            ss.end();
+                          }
+                        })
+                        .pipe(xmlParser);
 
-        that.state += 1;
-        if (that.state > that.end) {
-            clearInterval(interval);
-        }
-    }, that.delay);    
-}
+                    that.state += 1;
+                }
+            }
+        );
+    }
+    next();
+};
 
-module.exports = MetaUtil
+module.exports = MetaUtil;

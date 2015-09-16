9c8921f6bb6a60cd8b95c9ae697fe9f46b129b47
Colin Broderick <rusty@therustyproject.com>
2015-09-16 09:39:01 +0100

    updated stats

--- get_monthly_stats.py
+++ get_monthly_stats.py
@@ -78,13 +78,13 @@ if len(les_apps) > 0:
 	ap_us['total_edits'] = ap_us['create'] + ap_us['modify'] + ap_us['delete']	
 			# get last 30 days
 	d = datetime.datetime.today().strftime("%m/%d/%Y")
-	d30 = datetime.datetime.today() + datetime.timedelta(-30)
+	d30 = datetime.datetime.today() + datetime.timedelta(-3)
 	d30 = d30.strftime("%m/%d/%Y")
 	app_30edits = les_apps[d30:d][['user', 'type']].groupby(['user']).size()
 
 		#ap_us.reset_index(col_level=0, inplace=True)
 	ap_us = pd.merge(ap_us.reset_index(), app_30edits.reset_index(), on='user', how='outer').set_index('user')
-	ap_us.rename(columns={0:'last_30days'}, inplace=True)
+	ap_us.rename(columns={0:'last_3_days'}, inplace=True)
 	ap_us.fillna(0,inplace=True)
 	#ap_us.drop('type', axis=1, inplace=True)
 	#
@@ -92,7 +92,7 @@ if len(les_apps) > 0:
 	#ap_us = ap_us.reset_index()
 	#ap_us.loc[len(ap_us)] = list(np.append(['Mpaleng'], new_tots))
 	#ap_us.set_index('user', inplace=True)
-	ap_us[['create', 'delete', 'modify', 'total_edits', 'last_30days']] = ap_us[['create', 'delete', 'modify', 'total_edits', 'last_30days']].astype('float')
+	ap_us[['create', 'delete', 'modify', 'total_edits', 'last_30days']] = ap_us[['create', 'delete', 'modify', 'total_edits', 'last_3_days']].astype('float')
 	#reset dtypes to numbers
 	#ap_us[['total_edits']].sort('total_edits').plot(kind='barh', stacked=True, title="APP Total Edits", figsize=(20,20)).get_figure().savefig('app_total_edits.png')
 	#ap_us.sort('total_edits')[['create', 'modify', 'delete']].plot(kind='barh', stacked=True, title="APP Edits by Type", figsize=(20,20)).get_figure().savefig('app_edits_by_type.png')

40f591615495be4a30441e9ab2b734c566b43582
Colin Broderick <rusty@therustyproject.com>
2015-10-20 13:41:54 +0200

    added new bash script

---
+++ stats.sh
@@ -0,0 +1,12 @@
+#TODAY=date +%Y-%m-%d
+TODAY=`date +%Y-%m-%d`
+
+echo "Today is '$TODAY'"
+
+python get_maplesotho_stats.py --stats
+
+git add data
+
+git commit -am "Updated stats with '$TODAY'"
+
+git push

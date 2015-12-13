d0121aae32689d95d51781b4859b122746cbe868
Colin Broderick <rusty@therustyproject.com>
2015-12-13 22:02:41 +0100

    updated the stats page layout to add in all older stats

--- get_monthly_stats.py
+++ get_monthly_stats.py
@@ -12,7 +12,7 @@ import datetime
 #pd.options.display.mpl_style = 'default' # set styles to nice colours for graphs
 
 # read files in data folder for existing files:
-def read_files(place="lesotho"):
+def read_json_files(place="lesotho"):
     wd0 = 'data/'
     if place == 'lesotho':
         wd = wd0 + 'lesotho1/'
@@ -27,16 +27,41 @@ def read_files(place="lesotho"):
     else:
         #sys.exit("make sure that json.new files exist in the data/lesotho directory.")
         print("fcheck failes")
+
+# read files in data folder for existing files:
+def read_files(place="lesotho"):
+    wd0 = 'data/'
+    if place == 'lesotho':
+        wd = wd0 + 'lesotho/'
+    else:
+        wd = wd0
+    files = os.listdir (wd) # from /src
+    fcheck = any (f.find ("lesotho") > -1 and f.endswith(".json.new") for f in files)
+    if fcheck:
+        json_files = [wd + i for i in files if i.endswith('.json.new')]
+        print('Found %s geojson.new files in directory...' % len(json_files))
+    else:
+        sys.exit("make sure that json.new files exist in the data/lesotho directory.")
+    return json_files
     
 
-dfiles = read_files()
+dfiles_h = read_json_files()
+dfiles_d = read_files() # read old daily
 
-les0 = []
-for f in dfiles:
-    les0.append(pd.read_json(f))
+les_daily = []
+for f in dfiles_d:
+    les_daily.append(pd.read_json(f))
 
+les_hourly = []
+for f in dfiles_h:
+    les_hourly.append(pd.read_json(f))
 
-lesa = pd.concat(les0)
+lesh = pd.concat(les_hourly)
+# fix the naming of columns so they're the same
+lesh = lesh.rename(columns = {'id':'changeset'})
+lesd = pd.concat(les_daily)
+
+lesa = pd.concat([lesh, lesd])
 print("Dataframe with %s rows x %s columns" % (lesa.shape[0], lesa.shape[1]))
 
 lesa.timestamp = pd.to_datetime(lesa['timestamp']) # convert timestamp to date time index
@@ -44,20 +69,24 @@ lesa.timestamp = pd.to_datetime(lesa['timestamp']) # convert timestamp to date t
 #lesa.replace(['Miss O', 'The Big C'], ['Mpaleng Oliphant', 'DeBigC'], inplace=True)
 
 lesa.set_index(lesa.timestamp, inplace=True)
-
+les_apps = lesa[lesa.index > '01-01-2015']
 ## get planner stats
 #apus = pd.read_csv('data/lesotho/app_unames.csv')
 #apps = apus.username[apus.is_app == 'Y']
 
+app_edits = les_apps[['user', 'type']].groupby(['user','type']).size()
+ap_us = app_edits.unstack().fillna(0)
+ap_us['total_edits'] = ap_us['create'] + ap_us['modify'] + ap_us['delete']  
+total_rank = ap_us.sort('total_edits', ascending=False).reset_index()
 #print("Returning monthly stats for %s Assistant Planners" % len(apps))
 #les_apps = lesa[lesa.user.isin(apps.values)]
 #les_apps.drop('name', axis=1, inplace=True) # drop column with name: [way,node]
 #les_apps = les_apps[les_apps['name'] != 'way'] # remove all the ways
-les_apps = lesa.rename(columns = {'id':'changeset'})
+#les_apps = lesa.rename(columns = {'id':'changeset'})
 min_ts = str(les_apps.index.min())
 max_ts = str(les_apps.index.max())
-total = les_apps.groupby(['user']).resample('A', how='count')[['changeset']].reset_index()
-total_rank = pd.DataFrame(total[['user','changeset']]).sort('changeset', ascending=False)
+#total = les_apps.groupby(['user']).resample('A', how='count')[['changeset']].reset_index()
+#total_rank = pd.DataFrame(total[['user','changeset']]).sort('changeset', ascending=False)
 total_rank.index = np.arange(1, len(total_rank)+1)
 table = pd.DataFrame(total_rank).to_html()
 table = table.replace('border="1"', '')
@@ -87,20 +116,52 @@ html_string = '''
     /*.dataframe tbody tr:nth-child(-n+10) th {
     color: #FFF;
     }*/
+    .top-caption {
+        font-size: 16px;
+        line-height: 1.25;
+        font-weight: 400;
+        text-transform: uppercase;
+        padding-top: 5px;
+        padding-bottom: 5px;
+        display: block;
+        padding-bottom: 30px;
+    }
+    span.top {
+        font-size: 3.5em;
+        font-weight: 600;
+        display: block;
+        clear: both;
+        line-height: 1;
+    }
+    .top.minor, .top-caption.minor {
+        font-size: 16px;
+        display: inline-block;
+        text-transform: capitalize;
+    }
+    .top-caption.first {
+        padding-bottom: 0;
+    }
     </style>
     <meta name='viewport' content='width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no' />
     <body>
         <div class="row text-center">
             <div class="small-8 small-centered columns text-center">
-                <h2>#MapLesotho Portmarnock CS 24H Mapathon</h2>
-                <p>These stats are updated once per hour. Use the table to track your ranking througout the mapathon.</a>.</p>
+                <h2>#MapLesotho - Numbers tracker</h2>
+                <p>These stats are updated once per hour. Use the table to track your ranking based on such an abritary thing,<br>simple the number of edits you've maded.</p>
             </div>
         </div>
         
         <div class="row ">
             <div class="small-8 small-centered columns">
                 <div class="text-center">
-                    <strong>#MapLesotho Portmarnock 24H Leaderboard</strong>
+                    <span class="top">'''+ '{:,}'.format(ap_us['total_edits'].sum().astype(int)) +'''</span>
+                    <span class="top-caption first">total since 1 FEB 2015</span>
+                    <span class="top minor">'''+ '{:,}'.format(ap_us['create'].sum().astype(int)) +'''</span>
+                    <span class="top-caption minor">created</span>
+                    <span class="top minor">'''+ '{:,}'.format(ap_us['modify'].sum().astype(int)) +'''</span>
+                    <span class="top-caption minor">modified</span>
+                    <span class="top minor ">'''+ '{:,}'.format(ap_us['delete'].sum().astype(int)) +'''</span>
+                    <span class="top-caption minor">created</span>
                     <br> <small>First Edit Counted: '''+ min_ts +'''</small>
                     <br> <small>Latest Edit Counted: '''+ max_ts +'''</small>
                 </div>
